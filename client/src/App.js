06bc2eb958bd76601458141f4566469a15f2717b
karim <karimyakoub1515@gmail.com>
2024-07-16 13:57:39 -0400

    added context for global user auth, and rebasing local changes to remote, cleaned routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext, useState}from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Cookies from 'js-cookie';
 import Layout from './components/Layout'
@@ -11,65 +11,32 @@ import AlbumsPage from './pages/AlbumsPage/AlbumsPage';
 import LoneAlbumPage from './pages/LoneAlbumPage/LoneAlbumPage';
 import ProfileHeader from './components/ProfileHeader';
 import UserReviewPage from './pages/profile/UserReviewPage';
+import UserFollowers from './pages/profile/UserFollowers';
 
-//TODO: Need to figure out how to incorporate user auth bc idt putting it in state works??
 
 
-export default function App(){
 
-  const [user, setUser] = React.useState(null)
-  const [loading, setLoading] = React.useState(true)
+export default function App(){  
 
-  React.useEffect(()=>{
-    async function checkAuth(){
-      const data = await fetch('http://localhost:8000/isauth/', {
-        method: 'GET',
-        credentials: 'include',
-        headers: {
-          "Content-Type": "application/json"
-      }
-      })
-      const dataCheck = await data.json()
-      console.log(dataCheck.userName)
-  
-      if(dataCheck.status !== 200){
-        setUser(null)
-        setLoading(false)
-      }
-      else{
-        setUser(dataCheck.userName)
-        setLoading(false)
-      }
-    }
-    checkAuth()
-  },[])
- 
-
-  
   return(
     <BrowserRouter>
-    
       <Routes>
-        <Route element={<Layout user={user} />}>
-          <Route index path='/' element={<Home  user = {user}/>}/>
-          <Route path='signup' element={<SignUp user={user} setUser ={setUser}/>}/>
-          <Route path='login' element={<LogIn/>} />
-          <Route path='albums' element={<AlbumsPage/>}/>
-          <Route path='albums/:album' element={<LoneAlbumPage user={user} />} />
         
 
-
-        <Route element={<ProfileHeader />}>
-          <Route index path='user/:user/' element={<Profile user={user}/>}/> 
-          <Route path='user/:user/reviews' element={<UserReviewPage/>}/>
-          <Route path='user/:user/listenlist' element={<UserListenList/>}/>
-        </Route>
-
-        {/*
-          Route for when user clicks any album on a user page goes to their review
-        */}
-
-        </Route>
+          <Route element={<Layout/>}>
+              <Route index path='/' element={<Home/>}/>
+              <Route path='signup' element={<SignUp/>}/>
+              <Route path='login' element={<LogIn/>} />
+              <Route path='albums' element={<AlbumsPage/>}/>
+              <Route path='albums/:album' element={<LoneAlbumPage/>} />
+            <Route element={<ProfileHeader />}>
+              <Route index path='user/:user/' element={<Profile/>}/> 
+              <Route path='user/:user/reviews' element={<UserReviewPage/>}/>
+              <Route path='user/:user/listenlist' element={<UserListenList/>}/>
+              <Route path='user/:user/followers' element={<UserFollowers/>}/>          
+            </Route>
+          </Route>
+        
       </Routes>
     
     </BrowserRouter>
