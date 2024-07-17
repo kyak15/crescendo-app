99cafa71c06f1fed59701030a776a9332fcd572c
karim <karimyakoub1515@gmail.com>
2024-07-16 22:36:24 -0400

    context created to prevent prop drilling and share user state if a user is signed in or not

---
+++ client/src/UserContext.js
@@ -0,0 +1,41 @@
+// AuthContext.js
+import React, { createContext, useState, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [userName, setUserName] = useState(null);
+
+  React.useEffect(()=>{
+    async function checkAuth(){
+  
+        const authCheck = await fetch('http://localhost:8000/isauth/', {
+          method: 'GET',
+          credentials: 'include',
+          headers: {
+            "Content-Type": "application/json"
+        }
+        })
+        const authData = await authCheck.json()
+
+        if(authData.status !== 200){
+            setUserName(null)   
+        }
+        else{
+            setUserName(authData.userName)
+        }
+    }
+
+    
+      checkAuth()
+    
+  },[]);
+
+  return (
+    <AuthContext.Provider value={{ userName, setUserName }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
