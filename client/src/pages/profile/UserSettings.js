015ca3b5c853410aa6644f19e093fec84427ea42
karim <karimyakoub1515@gmail.com>
2024-08-16 18:47:22 -0400

    added working fe elements for changing user data

--- client/src/pages/profile/UserSettings.js
+++ client/src/pages/profile/UserSettings.js
@@ -1,37 +1,134 @@
 import React, {useContext} from 'react'
-import { useParams, NavLink } from 'react-router-dom'
+import { useParams, NavLink, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../UserContext'
-
+const apiURL = process.env.REACT_APP_API_URL
 export default function UserSettings(){
     
+    const navigate = useNavigate()
     const { userName, setUserName } = useContext(AuthContext);
     const id = useParams().user
+    const [changeEmail, setChangeEmail] = React.useState({
+        oldEmail: '',
+        newEmail: ''
+    })
+    const [changePass, setChangePass] = React.useState({
+        oldPassword: '',
+        newPassword: ''
+    })
+    const [deletePass, setDeletePass] = React.useState()
+    
+    
+    async function handleEmailChange(e){
+        e.preventDefault()
+        const body = JSON.stringify(changeEmail)
+        try {
+            const emailChangeRequest = await fetch(`${apiURL}/api/newemail/`,{
+                method: 'PATCH',
+                credentials: 'include',
+                headers: {
+                  "Content-Type": "application/json"
+              },
+              body:body
+              })
+            
+            const emailChangeData = await emailChangeRequest.json()
+
+            if(emailChangeData.status !== 200){
+                return alert('Failure to Change Email')
+            }
+
+            return alert('Successfully Changed Email!')
+            
+            
+        } catch (error) {
+            return alert('Failure to Reach Server')
+        }
+        
+    }
+
+    async function handlePassChange(e){
+        e.preventDefault()
+        const body = JSON.stringify(changePass)
+        try {
+            const passChangeRequest = await fetch(`${apiURL}/api/changepassword/`,{
+                method: 'PATCH',
+                credentials: 'include',
+                headers: {
+                  "Content-Type": "application/json"
+              },
+              body:body
+              })
+            
+            const passChangeData = await passChangeRequest.json()
+
+            if(passChangeData.status !== 200){
+                return alert('Failure to Change Password')
+            }
+
+            alert('Successfully Changed Password!')
+            
+            
+        } catch (error) {
+            return alert('Failure to Reach Server')
+        }
+    }
+
+    async function handleDelete(e){
+        e.preventDefault()
+        
+        
+        
+        try {
+            const body = JSON.stringify(deletePass)
+            const passChangeRequest = await fetch(`${apiURL}/api/deleteuser/`,{
+                method: 'DELETE',
+                credentials: 'include',
+                headers: {
+                  "Content-Type": "application/json"
+              },
+              body:body
+              })
+            
+            const passChangeData = await passChangeRequest.json()
+
+            if(passChangeData.status !== 200){
+                return alert('Failure to Delete Account')
+            }
+
+            alert('Successfully Deleted Account')
+            return navigate('/')
+            
+            
+        } catch (error) {
+            return alert('Failure to Reach Server')
+        }
+    }
 
     return(
         <div className='settings-container'>
-            <h3>Under Construction</h3>
             
-            //*<div className='mini-container'>
-                //*<h3>Change Email:</h3>
-                //*<button>Change Email</button>
-            //*</div>
-//*
-            //*<div className='mini-container'>
-                //*<h3>Change UserName:</h3>
-                //*<button>Change UserName</button>
-            //*</div>
-//*
-            //*<div className='mini-container'>
-                //*<h3>Change Password:</h3>
-                //*<button>Change Password</button>
-            //*</div>
-//*
-            //*<div className='mini-container'>
-                //*<h3>Delete Account:</h3>
-                //*<button>Delete Account</button>
-            //*</div>
-//*            
-        //*</div>
+            <div className='mini-container'>
+                <h3>Change Email:</h3>
+                <textarea onChange={e =>setChangeEmail({...changeEmail, oldEmail:e.target.value})} placeholder='Enter Old Email'></textarea>
+                <textarea onChange={e =>setChangeEmail({...changeEmail, newEmail:e.target.value})} placeholder='Enter New Email'></textarea>
+                <button onClick={e => handleEmailChange(e)}>Submit Change</button>
+            </div>    
+
+
+                <div className='mini-container'>
+                <h3>Change Password:</h3>
+                <textarea onChange={e =>setChangePass({...changePass, oldPassword:e.target.value})} placeholder='Enter Old Password'></textarea>
+                <textarea onChange={e =>setChangePass({...changePass, newPassword:e.target.value})} placeholder='Enter New Password'></textarea>
+                <button onClick={e => handlePassChange(e)}>Submit Change</button>
+            </div>    
+
+                <div className='mini-container'>
+                <h3>Delete Account:</h3>
+                <textarea onChange={e=>setDeletePass(...deletePass, e.target.value)} placeholder='Enter Password'></textarea>
+                <button onClick={e => handleDelete(e)}>Submit Change</button>
+            </div>
+   
+        </div>
     )
 
 }
